68d802c3f336153f5c8e99bf2f205f9a0bfbc4e2
OsamaAlhosni <55885836+OsamaAlhosni@users.noreply.github.com>
2020-10-27 08:32:00 +0200

    update dash board

--- salary/employee/models.py
+++ salary/employee/models.py
@@ -7,18 +7,22 @@ class Mangement(models.Model):
 
     def __str__(self):
         return self.mangemen_name
+
+
 class EmportEmployee(models.Model):
     EmpID = models.IntegerField()
-    EmpName = models.CharField(max_length =150)
+    EmpName = models.CharField(max_length=150)
     MangementId = models.IntegerField()
 
     def __str__(self):
         return self.EmpName
-        
+
+
 class Employee(models.Model):
-    mangment = models.ForeignKey(Mangement,on_delete=models.CASCADE)
-    user =  models.OneToOneField(User,on_delete=models.CASCADE,null=False)
-    moblie_no =models.CharField(max_length=15,null=False,blank=False,default=None)
+    mangment = models.ForeignKey(Mangement, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    moblie_no = models.CharField(
+        max_length=15, null=False, blank=False, default=None)
     send_text = models.BooleanField(default=True)
     send_voice = models.BooleanField(default=True)
     send_email = models.BooleanField(default=False)
@@ -26,29 +30,27 @@ class Employee(models.Model):
     def __str__(self):
         return self.user.username
 
-class Salary(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE, null=False)
-    emmpname = models.CharField(max_length=250,blank =True, null=True)
-    sMonth = models.IntegerField(blank=False,null=False)
-    sYear = models.IntegerField(blank= False, null=False)
-    base_salary = models.DecimalField(max_digits=9,decimal_places=3)
-    total_salary = models.DecimalField(max_digits=9,decimal_places=3)        
-    total_Diesc = models.DecimalField(max_digits=9,decimal_places=3)
-    net_salary = models.DecimalField(max_digits=9,decimal_places=3)
-    overtime = models.DecimalField(max_digits=9,decimal_places=3)
-    health_care = models.DecimalField(max_digits=9,decimal_places=3)
-    solfa1 = models.DecimalField(max_digits=9,decimal_places=3)
-    solfa2 = models.DecimalField(max_digits=9,decimal_places=3)
 
-    def __str__(self):
-        return  self.sMonth, self.sYear
+class Salary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    emmpname = models.CharField(max_length=250, blank=True, null=True)
+    sMonth = models.IntegerField(blank=False, null=False)
+    sYear = models.IntegerField(blank=False, null=False)
+    base_salary = models.DecimalField(max_digits=9, decimal_places=3)
+    total_salary = models.DecimalField(max_digits=9, decimal_places=3)
+    total_Diesc = models.DecimalField(max_digits=9, decimal_places=3)
+    net_salary = models.DecimalField(max_digits=9, decimal_places=3)
+    overtime = models.DecimalField(max_digits=9, decimal_places=3)
+    health_care = models.DecimalField(max_digits=9, decimal_places=3)
+    solfa1 = models.DecimalField(max_digits=9, decimal_places=3)
+    solfa2 = models.DecimalField(max_digits=9, decimal_places=3)
 
 
 class Loan(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False)
-    loan = models.DecimalField(max_digits=9,decimal_places=3)
-    balance = models.DecimalField(max_digits=9,decimal_places=3)
-    disc_amount = models.DecimalField(max_digits=9,decimal_places=3)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    loan = models.DecimalField(max_digits=9, decimal_places=3)
+    balance = models.DecimalField(max_digits=9, decimal_places=3)
+    disc_amount = models.DecimalField(max_digits=9, decimal_places=3)
 
     def __str__(self):
         return self.user.first_name
--- salary/employee/views.py
+++ salary/employee/views.py
@@ -1,52 +1,65 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.db.models import Sum
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from .models import EmportEmployee
+from .models import EmportEmployee, Salary
+
 
 def home(request):
-    return render(request,'home.html')
-    
+    return render(request, 'home.html')
+
+
 def signupuser(request):
     if request.method == 'GET':
-       return render(request,'signup.html',{'form':UserCreationForm()})
+        return render(request, 'signup.html', {'form': UserCreationForm()})
     else:
         username = request.POST['username']
         password = request.POST['password1']
         password2 = request.POST['password2']
         if password2 == password:
             try:
-                user = User.objects.create_user(username,password=password)
+                user = User.objects.create_user(username, password=password)
                 user.save()
-                login(request,user)
+                login(request, user)
                 return redirect('dashboard')
-            except IntegrityError: 
-                return render(request,'signup.html',{'form':UserCreationForm(),'error':'هذا الرقم الوظيفي سبق تسجيله بالمنظومة'})
-                 
+            except IntegrityError:
+                return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'هذا الرقم الوظيفي سبق تسجيله بالمنظومة'})
+
         else:
             errmsg = 'كلمة المرور غير متطابقة'
-            return render(request,'signup.html',{'form':UserCreationForm(),'error':errmsg})
-# login user            
+            return render(request, 'signup.html', {'form': UserCreationForm(), 'error': errmsg})
+# login user
+
+
 def loginuser(request):
     if request.method == 'GET':
-        return render(request, 'login.html', {'form':AuthenticationForm()})
+        return render(request, 'login.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'login.html', {'form':AuthenticationForm(), 'error':'الرقم الوظيفي او كلمة المرور المدخلة غير صحيحين'})
+            return render(request, 'login.html', {'form': AuthenticationForm(), 'error': 'الرقم الوظيفي او كلمة المرور المدخلة غير صحيحين'})
         else:
             login(request, user)
             return redirect('dashboard')
+
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
+
+@login_required
 def dashboard(request):
-    return render(request,'dash.html')
+    user = request.user
+    salary = Salary.objects.filter(user=user)
+
+    return render(request, 'dash.html', {'salary': salary, 'user': user})
+
 
 def emport(request):
     if request.method == 'POST':
@@ -54,8 +67,9 @@ def emport(request):
         employees = EmportEmployee.objects.all()
         for employee in employees:
             username = str(employee.EmpID)
-            user = User.objects.create_user(username,password=password,first_name = employee.EmpName)
+            user = User.objects.create_user(
+                username, password=password, first_name=employee.EmpName)
             user.save()
-        return render(request,'emport.html',{'Msg': 'Emport end'})
+        return render(request, 'emport.html', {'Msg': 'Emport end'})
     else:
-        return render(request,'emport.html',{'Msg': 'Emport '})
+        return render(request, 'emport.html', {'Msg': 'Emport '})
