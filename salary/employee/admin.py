b79ad0822a7fec4f89c88cda8840db9ebcb3fb4d
OsamaAlhosni <55885836+OsamaAlhosni@users.noreply.github.com>
2020-11-19 23:51:26 +0200

    com

--- salary/employee/admin.py
+++ salary/employee/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Salary,Loan,Mangement,Employee
+from .models import Salary, Loan, Mangement, Employee
 
-admin.site.register(Salary)
+
+@admin.register(Salary)
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sMonth', 'sYear', 'net_salary',
+                    'overtime', 'health_care', 'solfa1', 'solfa2')
+    ordering = ('user', 'sMonth', 'sYear')
+    # search_fields = ('user', 'sMonth', 'sYear')
+
+
 admin.site.register(Loan)
 admin.site.register(Mangement)
 admin.site.register(Employee)
-
